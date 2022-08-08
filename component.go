02a1fb1a3bf1da234589247009c875953e2153f4
amatsagu <krzysztof.rapior@protonmail.com>
2022-08-08 19:36:38 +0200

    Fixed all component structs + added super struct to use them all!

--- component.go
+++ component.go
@@ -1,5 +1,12 @@
 package tempest
 
+// ==========================================================================================
+// QUICK INFO
+// Components are so messy because Discord API is really inconsistent in this section
+// and Golang team doesn't make it easier by refusing to add unions which so many people are asking for!
+// Feel free to make a pull request with better type definitions if you have idea how to improve it.
+// ==========================================================================================
+
 type ButtonStyle uint8
 
 const (
@@ -35,46 +42,66 @@ const (
 	COMPONENT_TEXT_INPUT
 )
 
-// Generic Component super struct
+type TextInputStyle uint8
+
+const (
+	TEXT_INPUT_SHORT     = iota + 1 // 	A single-line input.
+	TEXT_INPUT_PARAGRAPH            // A multi-line input.
+)
+
+// Generic Component super struct! Use "ButtonComponent", "SelectMenuComponent" or "TextInputComponent" whenever possible and this super struct as "any" component.
 type Component struct {
-	CustomId    string        `json:"custom_id,omitempty"`
-	Type        ComponentType `json:"type"`
-	Style       ButtonStyle   `json:"style,omitempty"`
-	Disabled    bool          `json:"disabled,omitempty"`
-	Label       string        `json:"label,omitempty"`
-	Emoji       *Emoji        `json:"emoji,omitempty"`
-	Url         string        `json:"url,omitempty"`
-	Placeholder string        `json:"placeholder,omitempty"`
-	MinValues   int           `json:"min_values,omitempty"`
-	MaxValues   int           `json:"max_values,omitempty"`
-	Options     []*Option     `json:"options,omitempty"`
-	Components  []*Component  `json:"components,omitempty"`
+	CustomId    string              `json:"custom_id,omitempty"`
+	Type        ComponentType       `json:"type"`
+	Style       ButtonStyle         `json:"style,omitempty"`
+	Label       string              `json:"label,omitempty"`
+	Emoji       *PartialEmoji       `json:"emoji,omitempty"`
+	Url         string              `json:"url,omitempty"`
+	Disabled    bool                `json:"disabled,omitempty"`
+	Placeholder string              `json:"placeholder,omitempty"`
+	MinValues   uint64              `json:"min_values,omitempty"`
+	MaxValues   uint64              `json:"max_values,omitempty"`
+	Required    bool                `json:"required,omitempty"`
+	Options     []*SelectMenuOption `json:"options,omitempty"`
+	Components  []*Component        `json:"components,omitempty"`
 }
 
 type ButtonComponent struct {
 	CustomId string        `json:"custom_id"`
-	Type     ComponentType `json:"type"`            // It gonna always be = 2 for button components.
+	Type     ComponentType `json:"type"` // It gonna always be = 2 for button components.
+	Style    ButtonStyle   `json:"style"`
 	Label    string        `json:"label,omitempty"` // Text label that appears on the button, max 80 characters.
 	Emoji    *PartialEmoji `json:"emoji,omitempty"`
-	Style    ButtonStyle   `json:"style"`
 	Url      string        `json:"url,omitempty"` // A url for link-style buttons.
 	Disabled bool          `json:"disabled,omitempty"`
 }
 
 type SelectMenuComponent struct {
 	CustomId    string              `json:"custom_id"`
-	Type        ComponentType       `json:"type"` // It gonna always be = 3 for select menu components.
-	Disabled    bool                `json:"disabled,omitempty"`
-	Placeholder string              `json:"placeholder,omitempty"` // Custom placeholder text if nothing is selected, max 150 characters
+	Type        ComponentType       `json:"type"`                  // It gonna always be = 3 for select menu components.
+	Placeholder string              `json:"placeholder,omitempty"` // Custom placeholder text if nothing is selected, max 150 characters.
 	MinValues   uint64              `json:"min_values,omitempty"`
 	MaxValues   uint64              `json:"max_values,omitempty"`
 	Options     []*SelectMenuOption `json:"options"`
+	Disabled    bool                `json:"disabled,omitempty"`
 }
 
 type SelectMenuOption struct {
-	Default     bool          `json:"default"`         // Whether to render this option as selected by default.
-	Label       string        `json:"label,omitempty"` // Text label that appears on the option label, max 80 characters.
-	Emoji       *PartialEmoji `json:"emoji,omitempty"`
+	Label       string        `json:"label,omitempty"`       // Text label that appears on the option label, max 80 characters.
 	Description string        `json:"description,omitempty"` // An additional description of the option, max 100 characters.
-	Value       string        `json:"value"`                 // Value to return back to app once clicked, max 100 characters.
+	Emoji       *PartialEmoji `json:"emoji,omitempty"`
+	Value       string        `json:"value"`   // Value to return back to app once clicked, max 100 characters.
+	Default     bool          `json:"default"` // Whether to render this option as selected by default.
+}
+
+type TextInputComponent struct {
+	CustomId    string         `json:"custom_id"`
+	Type        ComponentType  `json:"type"` // It gonna always be = 4 for text input components.
+	Style       TextInputStyle `json:"style"`
+	Label       string         `json:"label"`                 // Text label for text input, max 45 characters.
+	Placeholder string         `json:"placeholder,omitempty"` // Custom placeholder text if the input is empty, max 100 characters.
+	Value       string         `json:"value,omitempty"`       // A pre-filled value for this component, max 4000 characters.
+	MinValues   uint64         `json:"min_values,omitempty"`
+	MaxValues   uint64         `json:"max_values,omitempty"`
+	Required    bool           `json:"required,omitempty"` // Whether this component is required to be filled, default = true.
 }
