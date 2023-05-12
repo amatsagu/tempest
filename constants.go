b1212d37b07970200f3c58bf9826d92f040251a7
amatsagu <krzysztof.rapior@protonmail.com>
2023-05-12 21:24:30 +0200

    Sync with local files

--- constants.go
+++ constants.go
@@ -4,7 +4,8 @@ package tempest
 // var p_REST_NULL_SLICE_REPLACE []byte = []byte("[]")
 
 const (
-	DISCORD_API_URL = "https://discord.com/api/v10"
-	DISCORD_CDN_URL = "https://cdn.discordapp.com"
-	EPOCH           = 1420070400000 // Discord epoch in milliseconds
+	DISCORD_API_URL  = "https://discord.com/api/v10"
+	DISCORD_CDN_URL  = "https://cdn.discordapp.com"
+	EPOCH            = 1420070400000 // Discord epoch in milliseconds
+	ROOT_PLACEHOLDER = "-"
 )
--- util.go
+++ util.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"crypto/ed25519"
 	"encoding/hex"
-	"encoding/json"
 
 	"io"
 	"net/http"
@@ -54,94 +53,13 @@ func verifyRequest(r *http.Request, key ed25519.PublicKey) bool {
 	return ed25519.Verify(key, msg.Bytes(), sig)
 }
 
-func parseCommandsToDiscordObjects(commands map[string]map[string]Command, whitelist []string, reverseMode bool) []Command {
-	list := make([]Command, len(commands))
-	var itx uint32 = 0
-
-	for _, tree := range commands {
-		command := tree["-"]
-
-		for key, subCommand := range tree {
-			if key == "-" {
-				continue
-			}
-
-			command.Options = append(command.Options, Option{
-				Name:        subCommand.Name,
-				Description: subCommand.Description,
-				Type:        OPTION_SUB_COMMAND,
-				Options:     subCommand.Options,
-			})
-		}
-
-		list[itx] = command
-		itx++
-	}
-
-	wls := len(whitelist)
-	if wls == 0 {
-		return list
-	}
-
-	itx = 0
-
-	// Work as blacklist
-	if reverseMode {
-		filteredList := make([]Command, len(commands)-wls)
-
-		for itx, command := range list {
-			blocked := false
-			for _, cmdName := range whitelist {
-				if command.Name == cmdName {
-					blocked = true
-					break
-				}
-			}
-
-			if blocked {
-				continue
-			}
-
-			filteredList[itx] = command
-		}
-
-		return filteredList
-	}
-
-	// Work as whitelist
-	filteredList := make([]Command, wls)
-
-	for _, command := range list {
-		for _, cmdName := range whitelist {
-			if command.Name == cmdName {
-				filteredList[itx] = command
-				itx++
-			}
-		}
-	}
-
-	return filteredList
-}
-
 func terminateCommandInteraction(w http.ResponseWriter) {
-	body, err := sonnet.Marshal(Response{
-		Type: CHANNEL_MESSAGE_WITH_SOURCE_RESPONSE,
-		Data: &ResponseData{
+	body, err := sonnet.Marshal(ResponseMessage{
+		Type: CHANNEL_MESSAGE_WITH_SOURCE_RESPONSE_TYPE,
+		Data: &ResponseMessageData{
 			Content: "Oh snap! It looks like you tried to trigger (/) command which is not registered within local cache. Please report this bug to my master.",
+			Flags:   64,
 		},
-	})
-
-	if err != nil {
-		panic("failed to parse json payload")
-	}
-
-	w.Header().Add("Content-Type", "application/json")
-	w.Write(body)
-}
-
-func acknowledgeComponentInteraction(w http.ResponseWriter) {
-	body, err := json.Marshal(Response{
-		Type: DEFERRED_UPDATE_MESSAGE_RESPONSE,
 	})
 
 	if err != nil {
