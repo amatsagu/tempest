d08aaf627995469e479827bda1199a026dc8d6ab
amatsagu <krzysztof.rapior@protonmail.com>
2023-05-15 23:35:09 +0200

    feat: Add Rest client

---
+++ rest.go
@@ -0,0 +1,132 @@
+package tempest
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+type Rest struct {
+	sync.RWMutex
+	token      string
+	httpClient *http.Client
+	lockedTo   *time.Time
+}
+
+type rateLimitError struct {
+	Global     bool    `json:"global"`
+	Message    string  `json:"message"`
+	RetryAfter float32 `json:"retry_after"`
+}
+
+func (rest *Rest) Request(method string, route string, jsonPayload interface{}) ([]byte, error) {
+	rest.RWMutex.RLock()
+	if rest.lockedTo != nil {
+		timeLeft := time.Until(*rest.lockedTo)
+		rest.RWMutex.RUnlock()
+		if timeLeft > 0 {
+			time.Sleep(timeLeft)
+		}
+	}
+
+	for i := 1; i < 3; i++ {
+		raw, err, finished := rest.handleRequest(method, route, jsonPayload)
+		if finished {
+			return raw, err
+		}
+		time.Sleep(time.Microsecond * time.Duration(250*i))
+	}
+
+	return nil, errors.New("failed to make http request 3 times to " + method + " :: " + route + " (check internet connection and/or app credentials)")
+}
+
+func (rest *Rest) handleRequest(method string, route string, jsonPayload interface{}) ([]byte, error, bool) {
+	var req *http.Request
+	if jsonPayload == nil {
+		request, err := http.NewRequest(method, DISCORD_API_URL+route, nil)
+		if err != nil {
+			return nil, errors.New("failed to initialize new request: " + err.Error()), false
+		}
+		req = request
+	} else {
+		body, err := json.Marshal(jsonPayload)
+		if err != nil {
+			return nil, errors.New("failed to parse provided payload (make sure it's in JSON format)"), true
+		}
+
+		request, err := http.NewRequest(method, DISCORD_API_URL+route, bytes.NewBuffer(body))
+		if err != nil {
+			return nil, errors.New("failed to initialize new request: " + err.Error()), false
+		}
+		req = request
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("User-Agent", USER_AGENT)
+	req.Header.Add("Authorization", rest.token)
+
+	res, err := rest.httpClient.Do(req)
+	if err != nil {
+		return nil, errors.New("failed to process request: " + err.Error()), false
+	}
+
+	if res.StatusCode == 204 {
+		return nil, nil, true
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, errors.New("failed to parse response body (json): " + err.Error()), true
+	}
+
+	fmt.Println(res.StatusCode)
+
+	if res.StatusCode == 429 {
+		rateErr := rateLimitError{}
+		json.Unmarshal(body, &rateErr)
+
+		rest.RWMutex.Lock()
+		t := time.Now().Add(time.Second * time.Duration(rateErr.RetryAfter+5))
+		rest.lockedTo = &t
+		rest.RWMutex.Unlock()
+
+		time.Sleep(time.Until(*rest.lockedTo))
+
+		rest.RWMutex.Lock()
+		rest.lockedTo = nil
+		rest.RWMutex.Unlock()
+		return nil, errors.New("rate limit"), false
+	} else if res.StatusCode >= 400 {
+		return nil, errors.New(res.Status + " :: " + string(body)), true
+	}
+
+	return body, nil, true
+}
+
+func NewRest(token string) Rest {
+	if !strings.HasPrefix(token, "Bot ") {
+		panic("app token needs to start with \"Bot \" prefix (example: \"Bot XYZABCQEWQ\")")
+	}
+
+	return Rest{
+		token:      token,
+		httpClient: http.DefaultClient,
+	}
+}
+
+func NewCustomRest(token string, client *http.Client) Rest {
+	if !strings.HasPrefix(token, "Bot ") {
+		panic("app token needs to start with \"Bot \" prefix (example: \"Bot XYZABCQEWQ\")")
+	}
+
+	return Rest{
+		token:      token,
+		httpClient: client,
+	}
+}
