e2d9ed7e9cf88ec5acfc928ac854d10aad076a9b
amatsagu <krzysztof.rapior@protonmail.com>
2023-05-23 22:09:45 +0200

    docs: update some constants to be for only internal use

--- constant.go
+++ constant.go
@@ -12,7 +12,13 @@ const (
 
 // Prepare those replies as they never change so there's no point in re-creating them each time.
 var (
-	PING_RESPONSE_RAW_BODY            = []byte(fmt.Sprintf(`{"type":%d}`, PONG_RESPONSE_TYPE))
-	ACKNOWLEDGE_RESPONSE_RAW_BODY     = []byte(fmt.Sprintf(`{"type":%d}`, DEFERRED_UPDATE_MESSAGE_RESPONSE_TYPE))
-	UNKNOWN_COMMAND_RESPONSE_RAW_BODY = []byte(fmt.Sprintf(`{"type":%d,"data":{"content":"Oh snap! It looks like you tried to trigger (/) unknown command. Please report this bug to bot owner.","flags":64}}`, CHANNEL_MESSAGE_WITH_SOURCE_RESPONSE_TYPE))
+	private_PING_RESPONSE_RAW_BODY            = []byte(fmt.Sprintf(`{"type":%d}`, PONG_RESPONSE_TYPE))
+	private_ACKNOWLEDGE_RESPONSE_RAW_BODY     = []byte(fmt.Sprintf(`{"type":%d}`, DEFERRED_UPDATE_MESSAGE_RESPONSE_TYPE))
+	private_UNKNOWN_COMMAND_RESPONSE_RAW_BODY = []byte(fmt.Sprintf(`{"type":%d,"data":{"content":"Oh snap! It looks like you tried to trigger (/) unknown command. Please report this bug to bot owner.","flags":64}}`, CHANNEL_MESSAGE_WITH_SOURCE_RESPONSE_TYPE))
+)
+
+// Those are used to replace seemingly empty slice into empty array after marsalling struct to json string.
+var (
+	private_REST_NULL_SLICE_FIND    []byte = []byte("[null]")
+	private_REST_NULL_SLICE_REPLACE []byte = []byte("[]")
 )
