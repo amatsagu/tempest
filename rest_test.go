2956780a371598f7ba4b260d66a14552c4767810
amatsagu <krzysztof.rapior@protonmail.com>
2023-05-23 18:35:29 +0200

    test: improve error output

--- rest_test.go
+++ rest_test.go
@@ -10,18 +10,18 @@ import (
 // Spams any request to check for Rest race conditions.
 func TestRest(t *testing.T) {
 	rest := NewRest("Bot " + os.Getenv("BOT_TOKEN"))
-	go requestGateway(rest)
-	go requestGateway(rest)
-	go requestGateway(rest)
-	requestGateway(rest)
-	requestGateway(rest)
-	requestGateway(rest)
+	go requestGateway(rest, t)
+	go requestGateway(rest, t)
+	go requestGateway(rest, t)
+	requestGateway(rest, t)
+	requestGateway(rest, t)
+	requestGateway(rest, t)
 }
 
-func requestGateway(rest *Rest) {
+func requestGateway(rest *Rest, t *testing.T) {
 	body, err := rest.Request(http.MethodGet, "/gateway/bot", nil)
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 	fmt.Println(string(body))
 }
