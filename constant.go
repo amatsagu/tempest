b83b36caa98cabf29c066f0e4688b54e095832b2
amatsagu <krzysztof.rapior@protonmail.com>
2023-11-25 20:22:49 +0100

    feat: declare static values

---
+++ constant.go
@@ -0,0 +1,9 @@
+package tempest
+
+const (
+	DISCORD_API_URL  = "https://discord.com/api/v10"
+	DISCORD_CDN_URL  = "https://cdn.discordapp.com"
+	USER_AGENT       = "DiscordApp https://github.com/Amatsagu/tempest"
+	EPOCH            = 1420070400000 // Discord epoch in milliseconds
+	ROOT_PLACEHOLDER = "-"
+)
