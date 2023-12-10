6638c501889b86ce31eb85e10cac6d301ee05006
amatsagu <krzysztof.rapior@protonmail.com>
2023-12-10 17:06:24 +0100

    fix: bring back component & modal register methods

--- client-registry.go
+++ client-registry.go
@@ -41,6 +41,41 @@ func (client *Client) RegisterSubCommand(subCommand Command, rootCommandName str
 	return nil
 }
 
+// Bind function to all components with matching custom ids. App will automatically run bound function whenever receiving component interaction with matching custom id.
+func (client *Client) RegisterComponent(customIDs []string, fn func(*ComponentInteraction)) error {
+	if client.State() != INIT_STATE {
+		return errors.New("client is no longer in initialization state (avoid editing client's internals after it launches)")
+	}
+
+	for _, ID := range customIDs {
+		_, exists := client.components[ID]
+		if exists {
+			return errors.New("client already has registered \"" + ID + "\" component (custom id already in use)")
+		}
+	}
+
+	for _, ID := range customIDs {
+		client.components[ID] = fn
+	}
+
+	return nil
+}
+
+// Bind function to modal with matching custom id. App will automatically run bound function whenever receiving modal interaction with matching custom id.
+func (client *Client) RegisterModal(customID string, fn func(*ModalInteraction)) error {
+	if client.State() != INIT_STATE {
+		return errors.New("client is no longer in initialization state (avoid editing client's internals after it launches)")
+	}
+
+	_, exists := client.modals[customID]
+	if exists {
+		return errors.New("client already has registered \"" + customID + "\" modal (custom id already in use)")
+	}
+
+	client.modals[customID] = fn
+	return nil
+}
+
 // Sync currently cached slash commands to discord API. By default it'll try to make (bulk) global update (limit 100 updates per day), provide array with guild id snowflakes to update data only for specific guilds.
 // You can also add second param -> slice with all command names you want to update (whitelist). There's also third, boolean param that when = true will reverse wishlist to work as blacklist.
 func (client *Client) SyncCommands(guildIDs []Snowflake, whitelist []string, switchMode bool) error {
