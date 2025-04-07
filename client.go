a5e33d1ecb85943cdaba41df11f0cdb92232732a
amatsagu <krzysztof.rapior@protonmail.com>
2025-04-07 14:24:50 +0200

    feat: add few common client methods

--- client.go
+++ client.go
@@ -3,7 +3,12 @@ package ashara
 import (
 	"crypto/ed25519"
 	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"os"
 	"sync"
+	"time"
 )
 
 // Client is the core Ashara entrypoint
@@ -50,4 +55,102 @@ func NewClient(opt ClientOptions) Client {
 			},
 		},
 	}
+}
+
+// Pings Discord API and returns time it took to get response.
+func (client *Client) Ping() time.Duration {
+	start := time.Now()
+	client.Rest.Request(http.MethodGet, "/gateway", nil)
+	return time.Since(start)
+}
+
+func (client *Client) SendMessage(channelID Snowflake, message Message, files []*os.File) (Message, error) {
+	raw, err := client.Rest.RequestWithFiles(http.MethodPost, "/channels/"+channelID.String()+"/messages", message, files)
+	if err != nil {
+		return Message{}, err
+	}
+
+	res := Message{}
+	err = json.Unmarshal(raw, &res)
+	if err != nil {
+		return Message{}, errors.New("failed to parse received data from discord")
+	}
+
+	return res, nil
+}
+
+func (client *Client) SendLinearMessage(channelID Snowflake, content string) (Message, error) {
+	return client.SendMessage(channelID, Message{Content: content}, nil)
+}
+
+// Creates (or fetches if already exists) user's private text channel (DM) and tries to send message into it.
+// Warning! Discord's user channels endpoint has huge rate limits so please reuse Message#ChannelID whenever possible.
+func (client *Client) SendPrivateMessage(userID Snowflake, content Message, files []*os.File) (Message, error) {
+	res := make(map[string]interface{}, 0)
+	res["recipient_id"] = userID
+
+	raw, err := client.Rest.Request(http.MethodPost, "/users/@me/channels", res)
+	if err != nil {
+		return Message{}, err
+	}
+
+	err = json.Unmarshal(raw, &res)
+	if err != nil {
+		return Message{}, errors.New("failed to parse received data from discord")
+	}
+
+	channelID, err := StringToSnowflake(res["id"].(string))
+	if err != nil {
+		return Message{}, err
+	}
+
+	msg, err := client.SendMessage(channelID, content, files)
+	msg.ChannelID = channelID // Just in case.
+
+	return msg, err
+}
+
+func (client *Client) EditMessage(channelID Snowflake, messageID Snowflake, content Message) error {
+	_, err := client.Rest.Request(http.MethodPatch, "/channels/"+channelID.String()+"/messages/"+messageID.String(), content)
+	return err
+}
+
+func (client *Client) DeleteMessage(channelID Snowflake, messageID Snowflake) error {
+	_, err := client.Rest.Request(http.MethodDelete, "/channels/"+channelID.String()+"/messages/"+messageID.String(), nil)
+	return err
+}
+
+func (client *Client) CrosspostMessage(channelID Snowflake, messageID Snowflake) error {
+	_, err := client.Rest.Request(http.MethodPost, "/channels/"+channelID.String()+"/messages/"+messageID.String()+"/crosspost", nil)
+	return err
+}
+
+func (client *Client) FetchUser(id Snowflake) (User, error) {
+	raw, err := client.Rest.Request(http.MethodGet, "/users/"+id.String(), nil)
+	if err != nil {
+		return User{}, err
+	}
+
+	res := User{}
+	err = json.Unmarshal(raw, &res)
+	if err != nil {
+		return User{}, errors.New("failed to parse received data from discord")
+	}
+
+	return res, nil
+}
+
+func (client *Client) FetchMember(guildID Snowflake, memberID Snowflake) (Member, error) {
+	raw, err := client.Rest.Request(http.MethodGet, "/guilds/"+guildID.String()+"/members/"+memberID.String(), nil)
+	if err != nil {
+		return Member{}, err
+	}
+
+	res := Member{}
+	err = json.Unmarshal(raw, &res)
+	if err != nil {
+		return Member{}, errors.New("failed to parse received data from discord")
+	}
+
+	return res, nil
 }
