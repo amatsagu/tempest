6d039c1eb238e81914754a483e6f08d034149392
amatsagu <krzysztof.rapior@protonmail.com>
2025-07-22 00:44:37 +0200

    fix: add custom unmarshal logic for ModalSubmitData to make modal handlers work again

--- interaction-json.go
+++ interaction-json.go
@@ -1,1 +1,30 @@
 package tempest
+
+import "encoding/json"
+
+func (msd *ModalSubmitData) UnmarshalJSON(data []byte) error {
+	type alias ModalSubmitData
+	var raw struct {
+		alias
+		Components []json.RawMessage `json:"components"`
+	}
+
+	if err := json.Unmarshal(data, &raw); err != nil {
+		return err
+	}
+
+	*msd = ModalSubmitData(raw.alias)
+	for _, comp := range raw.Components {
+		parsed, err := UnmarshalComponent(comp)
+		if err != nil {
+			return err
+		}
+
+		if cmp, ok := parsed.(LayoutComponent); ok {
+			msd.Components = append(msd.Components, cmp)
+		}
+	}
+
+	raw.Components = nil
+	return nil
+}
