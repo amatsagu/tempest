47e52cf0dc5aed4a20a5fda5ea82b94ed71d2197
amatsagu <krzysztof.rapior@protonmail.com>
2025-07-22 22:41:59 +0200

    feat: update the way modal is read to show new helper function in use

--- example/internal/command/modal.go
+++ example/internal/command/modal.go
@@ -35,7 +35,33 @@ var Modal tempest.Command = tempest.Command{
 }
 
 func HelloModal(itx tempest.ModalInteraction) {
-	value := itx.GetInputValue("example-text-input")
+	var value string
+	// if row, ok := itx.Data.Components[0].(tempest.ActionRowComponent); ok {
+	// 	if textInput, ok := row.Components[0].(tempest.TextInputComponent); ok {
+	//      // This if check is an example, not required in this example as it has only 1 component in total.
+	// 		if textInput.CustomID == "example-test-input" {
+	// 			value = textInput.Value
+	// 		}
+	// 	}
+	// }
+
+	// This is just an example that lib provides helped function to traverse component trees.
+	// In case you work with interaction that you know has just 1-2 interactive components, use higher commented code for better performance.
+	textInput, ok := tempest.FindInteractiveComponent(
+		itx.Data.Components,
+		func(cmp tempest.TextInputComponent) bool {
+			if cmp.CustomID == "example-test-input" {
+				return true
+			}
+
+			return false
+		},
+	)
+
+	if ok {
+		value = textInput.Value
+	}
+
 	if value == "" {
 		itx.AcknowledgeWithLinearMessage("Oh, how about trying pizza?", false)
 	} else {
