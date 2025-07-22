78174bfc6cffe8c4e103745ca5130e49878f2c66
amatsagu <krzysztof.rapior@protonmail.com>
2025-07-22 22:41:23 +0200

    feat: add helper function to easily read submit values from components

--- component-helper.go
+++ component-helper.go
@@ -4,13 +4,23 @@ package tempest
 //
 // Warning - This function may work recursively for deeply nested components & uses type casting so it's not exactly light to use everywhere.
 // Try using it only for massive interactions and do manual check for interactions with just 1 or 2 attached interactive components.
-func FindInteractiveComponent[T InteractiveComponent](components []AnyComponent, filter func(T) bool) (T, bool) {
+func FindInteractiveComponent[CC AnyComponent, T InteractiveComponent](components []CC, filter func(T) bool) (T, bool) {
 	var zero T
 
 	for _, cmp := range components {
 		switch cmp.Kind() {
+		case BUTTON_COMPONENT_TYPE,
+			STRING_SELECT_COMPONENT_TYPE,
+			TEXT_INPUT_COMPONENT_TYPE,
+			USER_SELECT_COMPONENT_TYPE,
+			ROLE_SELECT_COMPONENT_TYPE,
+			MENTIONABLE_SELECT_COMPONENT_TYPE,
+			CHANNEL_SELECT_COMPONENT_TYPE:
+			if casted, ok := any(cmp).(T); ok && filter(casted) {
+				return casted, true
+			}
 		case ACTION_ROW_COMPONENT_TYPE:
-			if row, ok := cmp.(ActionRowComponent); ok {
+			if row, ok := any(cmp).(ActionRowComponent); ok {
 				for _, icmp := range row.Components {
 					if casted, ok := icmp.(T); ok && filter(casted) {
 						return casted, true
@@ -18,7 +28,7 @@ func FindInteractiveComponent[T InteractiveComponent](components []AnyComponent,
 				}
 			}
 		case SECTION_COMPONENT_TYPE:
-			if section, ok := cmp.(SectionComponent); ok {
+			if section, ok := any(cmp).(SectionComponent); ok {
 				// Only button is InteractiveComponent for Accessory type.
 				if section.Accessory.Kind() == THUMBNAIL_COMPONENT_TYPE {
 					continue
@@ -29,13 +39,14 @@ func FindInteractiveComponent[T InteractiveComponent](components []AnyComponent,
 				}
 			}
 		case CONTAINER_COMPONENT_TYPE:
-			container, ok := cmp.(ContainerComponent)
+			container, ok := any(cmp).(ContainerComponent)
 			if !ok {
 				continue
 			}
 
 			return FindInteractiveComponent(container.Components, filter)
 		}
+
 	}
 
 	return zero, false
