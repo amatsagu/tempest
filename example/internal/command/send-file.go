c54133dfd2b52a447e723fcd3731118d96d11529
amatsagu <krzysztof.rapior@protonmail.com>
2025-07-23 13:34:02 +0200

    fix: delete old, test print messages used back in testing

--- example/internal/command/send-file.go
+++ example/internal/command/send-file.go
@@ -3,7 +3,6 @@ package command
 import (
 	"log"
 	"os"
-	"time"
 
 	tempest "github.com/amatsagu/tempest"
 )
@@ -12,16 +11,12 @@ var SendFile tempest.Command = tempest.Command{
 	Name:        "send-file",
 	Description: "Upload example image as message attachment.",
 	SlashCommandHandler: func(itx *tempest.CommandInteraction) {
-		start := time.Now()
-
 		imageFile, err := os.Open("./example-image.png")
 		if err != nil {
 			log.Println("failed to open image file:", err)
 			return
 		}
 		defer imageFile.Close()
-
-		log.Println("Opening files took:", time.Since(start))
 
 		files := []tempest.File{
 			{
@@ -33,10 +28,9 @@ var SendFile tempest.Command = tempest.Command{
 		err = itx.SendReply(tempest.ResponseMessageData{
 			Content: "This message should have attached files!",
 		}, false, files)
+
 		if err != nil {
 			log.Println("SendReply failed:", err)
 		}
-
-		log.Println("SendReply took:", time.Since(start))
 	},
 }
